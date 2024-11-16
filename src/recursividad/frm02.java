89f25e7d3f4b807d59dd87050106eb1239bd7cd6
Stivenjhoel <stivenalbornoz31a@gmail.com>
2024-11-16 15:05:24 -0500

    Update frm02.java

--- src/recursividad/frm02.java
+++ src/recursividad/frm02.java
@@ -1,1 +1,25 @@
+package recursividad;
+
+public class frm02 {
+
+    // Función iterativa que calcula a^b
+    public static int potenciaIterativa(int base, int exponente) {
+        int resultado = 1; // Inicializamos el resultado en 1
+        
+        // Ciclo for para multiplicar la base por sí misma 'exponente' veces
+        for (int i = 1; i <= exponente; i++) {
+            resultado *= base;
+        }
+        
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        int base = 5;
+        int exponente = 3;
+        
+        int resultado = potenciaIterativa(base, exponente);
+        System.out.println(base + " elevado a " + exponente + " es: " + resultado);
+    }
+}
 
