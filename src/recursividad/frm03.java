5ff4279d558332a26140957151dda77287946f26
Stivenjhoel <stivenalbornoz31a@gmail.com>
2024-11-16 15:05:47 -0500

    Update frm03.java

--- src/recursividad/frm03.java
+++ src/recursividad/frm03.java
@@ -1,1 +1,22 @@
+package recursividad;
+
+public class frm03 {
+
+    // Función recursiva para calcular el n-ésimo número de Fibonacci
+    public static int fibonacci(int n) {
+        // Caso base: los dos primeros números de Fibonacci son 1
+        if (n <= 1) {
+            return 1;
+        }
+        // Caso recursivo: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 6; // Número en la serie Fibonacci que queremos calcular
+        
+        int resultado = fibonacci(n);
+        System.out.println("El número Fibonacci en la posición " + n + " es: " + resultado);
+    }
+}
 
